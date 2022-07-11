50bcdd8da0d02a8fdbdd6798615c673e83d555ad
rasheed <rasheed.shaik@thoughtworks.com>
2022-07-12 00:49:31 +0530

    [Abhishek / Rasheed] Add. Logic in service to save new password records

--- src/main/java/com/booking/passwordHistory/PasswordHistoryService.java
+++ src/main/java/com/booking/passwordHistory/PasswordHistoryService.java
@@ -2,10 +2,15 @@ package com.booking.passwordHistory;
 
 import com.booking.passwordHistory.repository.Constants;
 import com.booking.passwordHistory.repository.PasswordHistory;
+import com.booking.passwordHistory.repository.PasswordHistoryPK;
 import com.booking.passwordHistory.repository.PasswordHistoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.booking.users.User;
+import com.booking.users.UserRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.List;
 
 @Service
@@ -13,12 +18,24 @@ public class PasswordHistoryService {
 
     private PasswordHistoryRepository passwordHistoryRepository;
 
-    @Autowired
-    public PasswordHistoryService(PasswordHistoryRepository passwordHistoryRepository) {
+    private UserRepository userRepository;
+
+    public PasswordHistoryService(PasswordHistoryRepository passwordHistoryRepository, UserRepository userRepository) {
         this.passwordHistoryRepository = passwordHistoryRepository;
+        this.userRepository = userRepository;
     }
 
     public List<PasswordHistory> findRecentPasswordsByUserId(Long userId, Constants three) {
         return passwordHistoryRepository.findRecentPasswordsByUserIdWithLimit(userId, three.getValue());
+    }
+
+    public PasswordHistory add(Long userId, String password) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with this user id"));
+
+        PasswordHistoryPK passwordHistoryPK = new PasswordHistoryPK(user, password);
+        Timestamp createdAt = Timestamp.from(Instant.now());
+        PasswordHistory passwordHistory = new PasswordHistory(passwordHistoryPK, createdAt);
+        return passwordHistoryRepository.save(passwordHistory);
     }
 }
--- src/test/java/com/booking/passwordHistory/PasswordHistoryServiceTest.java
+++ src/test/java/com/booking/passwordHistory/PasswordHistoryServiceTest.java
@@ -4,30 +4,41 @@ import com.booking.passwordHistory.repository.PasswordHistory;
 import com.booking.passwordHistory.repository.PasswordHistoryPK;
 import com.booking.passwordHistory.repository.PasswordHistoryRepository;
 import com.booking.users.User;
+import com.booking.users.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static com.booking.passwordHistory.repository.Constants.THREE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 public class PasswordHistoryServiceTest {
 
     private PasswordHistoryRepository passwordHistoryRepository;
 
+    private UserRepository userRepository;
+
     private PasswordHistoryService passwordHistoryService;
+
+    private User user;
+    private PasswordHistory passwordHistory;
 
     @BeforeEach
     public void setUp() {
         passwordHistoryRepository = mock(PasswordHistoryRepository.class);
-        passwordHistoryService = new PasswordHistoryService(passwordHistoryRepository);
+        userRepository = mock(UserRepository.class);
+        passwordHistoryService = new PasswordHistoryService(passwordHistoryRepository, userRepository);
+        user = new User("test-user", "Password@123");
+        Timestamp instant = Timestamp.from(Instant.now());
+        passwordHistory = new PasswordHistory(new PasswordHistoryPK(user, "Password@12"), instant);
     }
 
     @Test
@@ -47,5 +58,24 @@ public class PasswordHistoryServiceTest {
         assertEquals(3, recentPasswords.size());
 
         verify(passwordHistoryRepository, times(1)).findRecentPasswordsByUserIdWithLimit(user.getId(), THREE.getValue());
+    }
+
+    @Test
+    void shouldBeAbleToSavePassword() {
+        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+        when(passwordHistoryRepository.save(passwordHistory)).thenReturn(any(PasswordHistory.class));
+
+        passwordHistoryService.add(user.getId(), passwordHistory.getPasswordHistoryPK().getPassword());
+
+        verify(passwordHistoryRepository, times(1)).save(any(PasswordHistory.class));
+    }
+
+    @Test
+    void shouldBeAbleToThrowWhenInvalidUserIdGiven() {
+        User user = new User("test-user", "Password@123");
+        when(userRepository.findById(user.getId())).thenThrow(new UsernameNotFoundException("User is not found"));
+
+        assertThrows(UsernameNotFoundException.class,
+                () -> passwordHistoryService.add(user.getId(), passwordHistory.getPasswordHistoryPK().getPassword()));
     }
 }
