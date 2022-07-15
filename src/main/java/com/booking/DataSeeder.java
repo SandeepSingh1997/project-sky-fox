759c15da01e3a3e677da13692acc1ae21adfb586
rasheed <rasheed.shaik@thoughtworks.com>
2022-07-15 15:36:09 +0530

    [Team01] Refactor. Set hashed password for seed user

--- src/main/java/com/booking/DataSeeder.java
+++ src/main/java/com/booking/DataSeeder.java
@@ -3,25 +3,35 @@ package com.booking;
 import com.booking.roles.repository.Role;
 import com.booking.users.repository.User;
 import com.booking.users.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class DataSeeder {
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Bean
     CommandLineRunner initDatabase(UserRepository repository) {
         return args -> {
             if (repository.findByUsername("seed-user-1").isEmpty()) {
-                repository.save(new User("seed-user-1", "Foobar@123", new Role("Admin")));
+                User user = new User("seed-user-1", "Foobar@123", new Role("Admin"));
+                user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+                repository.save(user);
             }
             if (repository.findByUsername("seed-user-2").isEmpty()) {
-                repository.save(new User("seed-user-2", "Foobar@124", new Role("Admin")));
+                User user = new User("seed-user-2", "Foobar@124", new Role("Admin"));
+                user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+                repository.save(user);
             }
             if (repository.findByUsername("seed-user-3").isEmpty()) {
-                repository.save(new User("seed-user-3", "Foobar@125", new Role("Admin")));
+                User user = new User("seed-user-3", "Foobar@125", new Role("Admin"));
+                user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+                repository.save(user);
             }
         };
     }
