43fc1249f97e00b986c0d2c7dc4bb6b14c917bba
antarikshray <antarikshray@gmail.com>
2022-07-15 13:18:26 +0530

    [Shravya | Sandeep | Antariksh] Add. Validation on request and no authorization for customers signup

--- src/main/java/com/booking/customer/CustomerSignupRequest.java
+++ src/main/java/com/booking/customer/CustomerSignupRequest.java
@@ -5,16 +5,21 @@ import com.booking.users.repository.User;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 
+import javax.validation.constraints.Pattern;
+
 public class CustomerSignupRequest {
     @JsonProperty
     @ApiModelProperty(name = "name", value = "customer  name", required = true, position = 1)
     private String name;
 
     @JsonProperty
+    @Pattern(regexp = "([a-zA-Z0-9+_.-]+@[a-zA-Z" +
+            "0-9.-]+$)", message = "Enter valid email")
     @ApiModelProperty(name = "email", value = "customer  email", required = true, position = 2)
     private String email;
 
     @JsonProperty
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Phone number must have exactly 10 digits")
     @ApiModelProperty(name = "phoneNumber", value = "customer  phone number", required = true, position = 3)
     private String phoneNumber;
 
@@ -23,6 +28,7 @@ public class CustomerSignupRequest {
     private String username;
 
     @JsonProperty
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[-@#$%^&+=])(?=\\S+$).{8,16}$", message = "Current password must contain atleast one Capital Letter, one Special character, one Digit, Minimum of 8 and Maximum of 16 characters")
     @ApiModelProperty(name = "password", value = "customer  password", required = true, position = 5)
     private String password;
 
